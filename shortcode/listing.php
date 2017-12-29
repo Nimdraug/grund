8c56347b1153d8cf0b9f341fd9c846facd5f00cb
Martin Hulten-Ashauer <martin@mortos.org>
2017-12-30 03:43:18 +1100

    new query for shortcode

--- shortcode/listing.php
+++ shortcode/listing.php
@@ -1,6 +1,16 @@
 <?php
 add_shortcode( 'listing', function ( $atts, $content ) {
-    global $wp_theme;
+    global $wp_theme, $wp_query;
+
+    $old_wp_query = $wp_query;
+
+    $wp_query = new WP_Query( [
+        'post_type' => 'post'
+    ] );
 
     $wp_theme->listing();
+
+    wp_reset_postdata();
+
+    $wp_query = $old_wp_query;
 } );
