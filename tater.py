e4224b428fff7e507a932dd2a63189d73ff7c426
Martin Hulten-Ashauer <martin@mortos.org>
2017-12-30 00:24:47 +1100

    basic deployment + build script

---
+++ tater.py
@@ -0,0 +1,29 @@
+import taters.locations
+import locations
+import re
+
+def ignore( files ):
+    for f in files:
+        if re.match( '(tater\.py.?|locations\.py(.?|\.example))' ):
+            continue
+
+        yield f
+
+def build( files ):
+    for f in files:
+        yield f
+
+def main( destination = 'local' ):
+    here = taters.locations.git( '.' )
+
+    there = getattr( locations, destination )
+
+    here_revision = here.get_ref_commit()
+
+    try:
+        there_revision = taters.read_all( there.get( '.taters-version' ) )
+    except Exception as e:
+        print e
+        there_revision = None
+
+    there.destination( taters.debug_filter( build( ignore( here.source( here_revision, there_revision, recursive = True ) ) ) ) )
