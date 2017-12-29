3a3a911e0624d7c4a061656f8634590378058fb5
Martin Hulten-Ashauer <martin@mortos.org>
2017-12-30 01:11:10 +1100

    stub theme class

---
+++ class/theme.php
@@ -0,0 +1,34 @@
+<?php
+
+class theme
+{
+    function __construct()
+    {
+    }
+
+    function the_layout()
+    {
+
+    }
+
+    function the_view()
+    {
+
+    }
+
+    function listing()
+    {
+
+    }
+
+    function listed()
+    {
+    }
+
+    function detail()
+    {
+
+    }
+}
+
+$wp_theme = theme();
