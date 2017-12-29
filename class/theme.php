178167bcc1bb394e37937899164164a868355d04
Martin Hulten-Ashauer <martin@mortos.org>
2017-12-30 02:32:20 +1100

    very basic view templating

--- class/theme.php
+++ class/theme.php
@@ -13,21 +13,27 @@ class theme
 
     function the_view()
     {
+        global $wp_query;
 
+        if ( $wp_query->is_archive() )
+            $this->listing();
+        else
+            $this->detail();
     }
 
     function listing()
     {
-
+        get_template_part( 'listing/listing' );
     }
 
     function listed()
     {
+        get_template_part( 'listed/listed' );
     }
 
     function detail()
     {
-
+        get_template_part( 'detail/detail' );
     }
 }
 
