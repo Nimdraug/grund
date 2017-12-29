e518bc832d2d705453b71f7eaad6a0618bb70227
Martin Hulten-Ashauer <martin@mortos.org>
2017-12-30 03:15:56 +1100

    link to detail view

--- listed/listed.php
+++ listed/listed.php
@@ -1,6 +1,6 @@
 <article>
     <header>
-        <h2><?php the_title() ?></h2>
+        <h2><a href="<?=get_the_permalink()?>"><?php the_title() ?></a></h2>
     </header>
     <div>
         <?php the_excerpt() ?>
