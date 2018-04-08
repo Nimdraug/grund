9205cae978b8e3a26c098080c560f2e73f873af6
Martin Hulten-Ashauer <martin@mortos.org>
2018-04-08 14:47:03 +1000

    clear up stuff now done by taters-cmd

--- tater.py
+++ tater.py
@@ -1,5 +1,4 @@
 import taters.locations
-import locations
 import re
 
 def ignore( files ):
@@ -12,27 +11,3 @@ def ignore( files ):
 def build( files ):
     for f in files:
         yield f
-
-def main( destination = 'local' ):
-    here = taters.locations.git( '.' )
-
-    there = getattr( locations, destination )
-
-    here_revision = here.get_ref_commit()
-
-    try:
-        there_revision = taters.read_all( there.get( '.taters-version' ) )
-    except Exception as e:
-        print e
-        there_revision = None
-
-    there.destination( build( ignore( taters.debug_filter( here.source( there_revision, None, recursive = True ) ) ) ), overwrite = True )
-
-    version_p = taters.pipe( '.taters-version' )
-    version_p.w.write( here_revision )
-    version_p.w.close()
-
-    there.put( version_p.r )
-
-if __name__ == '__main__':
-    main()
