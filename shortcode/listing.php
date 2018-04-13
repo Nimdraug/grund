e852afcb49fbe1196f171119b837666d504fa1a1
Martin Hulten-Ashauer <martin@mortos.org>
2018-04-13 14:43:08 +1000

    parse listing shortcode's atts correctly

--- shortcode/listing.php
+++ shortcode/listing.php
@@ -2,10 +2,10 @@
 add_shortcode( 'listing', function ( $atts, $content ) {
     global $grund_theme, $wp_query;
 
-    $atts = shortcode_atts( WP_Query::fill_query_vars( [
+    // grund default query vars + wp default query vars + shortcode defaults + given shortcode atts
+    $atts = shortcode_atts( wp_parse_args( [
         'post_type' => 'post',
-        'listing' => null
-    ] ), $atts );
+    ], WP_Query::fill_query_vars( $grund_theme->default_query_vars ) ), $atts );
 
     $grund_theme->push_query();
 
